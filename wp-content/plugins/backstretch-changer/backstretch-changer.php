e26289ea54d586917b0a9c3aeffacad747e601cd
shamelessApathy <bcm811@gmail.com>
2017-11-27 11:28:06 -0700

    couple changes, stuck on media adder default wp thing, branching off now

--- wp-content/plugins/backstretch-changer/backstretch-changer.php
+++ wp-content/plugins/backstretch-changer/backstretch-changer.php
@@ -8,22 +8,24 @@
    Author URI: http://slcutahdesign.com
    License: GPL2
    */
-add_action('admin_menu', 'my_cool_plugin_create_menu');
+add_action('admin_menu', 'backstretch_changer_create_menu');
 
-function my_cool_plugin_create_menu() {
+function backstretch_changer_create_menu() {
 
 	//create new top-level menu
-	add_menu_page('Backstretch-Changer Settings', 'Image Settings', 'administrator', __FILE__, 'backstretch_changer_settings_page' , plugins_url('/images/icon.png', __FILE__) );
+	add_menu_page('Backstretch-Changer Settings', 'Image Settings', 'administrator', __FILE__, 'backstretch_changer_settings_page' , plugins_url('/images/logo.png', __FILE__) );
 
 	//call register settings function
-	add_action( 'admin_init', 'register_my_cool_plugin_settings' );
+	add_action( 'admin_init', 'register_backstretch_changer_plugin_settings' );
 
 function register_backstretch_changer_plugin_settings() 
 {
 	//register our settings
-	register_setting( 'backstretch-changer-settings-group', 'new_option_name' );
-	register_setting( 'backstretch-changer-settings-group', 'some_other_option' );
-	register_setting( 'backstretch-changer-settings-group', 'option_etc' );
+	register_setting( 'backstretch-changer-settings-group', 'first_image' );
+	register_setting( 'backstretch-changer-settings-group', 'second_image' );
+  register_setting( 'backstretch-changer-settings-group', 'third_image' );
+  register_setting( 'backstretch-changer-settings-group', 'fourth_image' );
+	register_setting( 'backstretch-changer-settings-group', 'fifth_image' );
 }
 
 function backstretch_changer_settings_page() {
@@ -36,18 +38,28 @@ function backstretch_changer_settings_page() {
     <?php do_settings_sections( 'backstretch-changer-settings-group' ); ?>
     <table class="form-table">
         <tr valign="top">
-        <th scope="row">New Option Name</th>
-        <td><input type="text" name="new_option_name" value="<?php echo esc_attr( get_option('new_option_name') ); ?>" /></td>
+        <th scope="row">First Image</th>
+        <td><input type="text" name="First-Image" value="<?php echo esc_attr( get_option('First-Image') ); ?>" /></td>
         </tr>
          
         <tr valign="top">
-        <th scope="row">Some Other Option</th>
-        <td><input type="text" name="some_other_option" value="<?php echo esc_attr( get_option('some_other_option') ); ?>" /></td>
+        <th scope="row">Second Image</th>
+        <td><input type="text" name="Second-Image" value="<?php echo esc_attr( get_option('Second-Image') ); ?>" /></td>
         </tr>
         
         <tr valign="top">
-        <th scope="row">Options, Etc.</th>
-        <td><input type="text" name="option_etc" value="<?php echo esc_attr( get_option('option_etc') ); ?>" /></td>
+        <th scope="row">Thrid Image</th>
+        <td><input type="text" name="Third-Image" value="<?php echo esc_attr( get_option('Third-Image') ); ?>" /></td>
+        </tr>
+
+        <tr valign="top">
+        <th scope="row">Fourth Image</th>
+        <td><input type="text" name="Fourth-Image" value="<?php echo esc_attr( get_option('Fourth-Image') ); ?>" /></td>
+        </tr>
+
+        <tr valign="top">
+        <th scope="row">Fifth Image</th>
+        <td><input type="text" name="Fifth-Image" value="<?php echo esc_attr( get_option('Fifth-Image') ); ?>" /></td>
         </tr>
     </table>
     
